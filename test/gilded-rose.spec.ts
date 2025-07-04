97bdb32b771281cd44cd9191619f1eeace51a6a9
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-04 15:29:43 +0300

    Added general tests

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -2,16 +2,42 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
-
-    it('Checking if the quality of Sulfurus does not modify', function() {
-        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 50) ]);
+    //General tests
+    it('Checking if the selling day of a product except Sulfurus modifies', function() {
+        const gildedRose = new GildedRose([ new Item('Potato', 13, 12) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(50);
+        expect(items[0].sellIn).to.equal(12);
+    });
+
+    it('Checking once the sell by date has passed, quality degrades twice as fast, except Sulfurus', function() {
+        const gildedRose = new GildedRose([ new Item('Potato', -2, 12) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(10);
+    });
+
+    /** 
+    it('Checking if the added object exists in the inventory', function() {
+        const namesList = ['Sulfuras, Hand of Ragnaros', 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert']
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 0, 50) ]);
+        const items = gildedRose.updateQuality();
+        expect(namesList).to.include(items[0].name) ;
+    });
+    */ 
+
+    //Sulfurus tests
+    it('Checking if the quality of Sulfurus does not modify', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(80);
     });
     
     it('Checking if the selling day of Sulfurus does not modify', function() {
-        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 50) ]);
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].sellIn).to.equal(0);
     });
+
+    //Aged Brie tests
+    
+
 });
