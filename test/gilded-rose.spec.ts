cfb23b141a2b924a3442eed1700e46bff07b3271
Claudia Golaes <Claudia.Golaes@softwire.com>
2025-07-04 15:47:32 +0300

    Added Backstage Passes Tests

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
-    //General tests
+    // General tests
     it('Checking if the selling day of a product except Sulfurus modifies', function() {
         const gildedRose = new GildedRose([ new Item('Potato', 13, 12) ]);
         const items = gildedRose.updateQuality();
@@ -30,7 +30,7 @@ describe('Gilded Rose', function () {
     });
     */ 
 
-    //Sulfurus tests
+    // Sulfurus tests
     it('Checking if the quality of Sulfurus does not modify', function() {
         const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
         const items = gildedRose.updateQuality();
@@ -43,14 +43,37 @@ describe('Gilded Rose', function () {
         expect(items[0].sellIn).to.equal(0);
     });
 
-    //Aged Brie tests
+    // Aged Brie test
     it('Checking if the quality of Aged Brie increases once a day passes', function() {
         const gildedRose = new GildedRose([ new Item('Aged Brie', 5, 12) ]);
         const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(13);
     });
 
-    
+    // Backstage passes test 
+    it('Checking if the quality of the Backstage passes increases once a day passes', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 12, 12) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(13);
+    });
+
+    it('Checking if the quality of the Backstage passes drops to 0 after the concert', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 13) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+    });
+
+    it('Checking if the quality of the Backstage passes increases by 2 when 5 < sellIN <= 10', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 6, 13) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(15);
+    });
+
+    it('Checking if the quality of the Backstage passes increases by 3 when 0 < sellIN <= 5', function() {
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 1, 13) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(16);
+    });
     
 
 });
